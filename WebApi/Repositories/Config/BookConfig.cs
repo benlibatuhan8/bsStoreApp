613d48de7d1a11c7a11331f5fdd73cea686f9f64
Batuhan Benli <benlibatuhan6@hotmail.com>
2023-04-22 13:13:33 +0300

    BookConfig Eklendi cekirdek data migration ile eklenecek.

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id=1,Title="Yüzüncü Ad",Price=125},
+                new Book { Id=2,Title="Uçurtma Avcısı",Price=130},
+                new Book { Id=3,Title="Hayvanlar Çiftliği",Price=125}
+                );
+        }
+    }
+}
